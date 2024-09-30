5b36056f00fe8efd8f13d8f63abe0566bb55161a
mocchira <nekotaroh@gmail.com>
2024-09-30 13:29:28 +0900

    chore: make console.printFunc noop

--- src/ext/console/init_console.js
+++ src/ext/console/init_console.js
@@ -3,6 +3,10 @@ import * as console from 'ext:deno_console/01_console.js';
 import { applyToGlobal, nonEnumerable } from 'ext:rustyscript/rustyscript.js';
 applyToGlobal({
     console: nonEnumerable(
-      new console.Console((msg, level) => globalThis.Deno.core.print(msg, level > 1)),
+      new console.Console((_msg, _level) => {
+          // noop
+          // TODO(ysh)
+          // This is a temporary solution to get rid of non-JSON output from log
+      }),
     ),
 });

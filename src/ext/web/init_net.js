113028f631540580ac7b82bf738deb8ee6d48e11
mocchira <nekotaroh@gmail.com>
2024-11-05 16:44:57 +0900

    chore(ext/web): make net/tls visible from globalThis

--- src/ext/web/init_net.js
+++ src/ext/web/init_net.js
@@ -1,3 +1,10 @@
 
 import * as net from "ext:deno_net/01_net.js";
 import * as tls from "ext:deno_net/02_tls.js";
+
+import {applyToGlobal, nonEnumerable} from 'ext:rustyscript/rustyscript.js';
+
+applyToGlobal({
+    net: nonEnumerable(net),
+    tls: nonEnumerable(tls)
+});

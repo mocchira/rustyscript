b58112d446ed93c428a7e1618e062401574235d5
mocchira <nekotaroh@gmail.com>
2024-12-23 09:53:00 +0900

    chore(src/ext): fix console.log

--- src/ext/console/init_console.js
+++ src/ext/console/init_console.js
@@ -3,10 +3,10 @@ import * as console from 'ext:deno_console/01_console.js';
 import { applyToGlobal, nonEnumerable } from 'ext:rustyscript/rustyscript.js';
 applyToGlobal({
     console: nonEnumerable(
-      new console.Console((_msg, _level) => {
-          // noop
+      new console.Console((msg, _level) => {
           // TODO(ysh)
           // This is a temporary solution to get rid of non-JSON output from log
+          rustyscript.functions['console.log'](msg);
       }),
     ),
 });

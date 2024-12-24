0674abb0eaec745afa91eb00968238e4dd682223
mocchira <nekotaroh@gmail.com>
2024-12-24 16:06:46 +0900

    fix(src): make net/tls invisible

--- src/ext/web/init_net.js
+++ src/ext/web/init_net.js
@@ -1,10 +1,3 @@
 
 import * as net from "ext:deno_net/01_net.js";
 import * as tls from "ext:deno_net/02_tls.js";
-
-import {applyToGlobal, nonEnumerable} from 'ext:rustyscript/rustyscript.js';
-
-applyToGlobal({
-    net: nonEnumerable(net),
-    tls: nonEnumerable(tls)
-});
